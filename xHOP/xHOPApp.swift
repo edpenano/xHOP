c4b2193b71b079970782e384be94135edd5750a2
Ed Penano <hi@edpenano.com>
2021-06-09 13:20:34 -0700

    Initial Commit

---
+++ xHOP/xHOPApp.swift
@@ -0,0 +1,17 @@
+//
+//  xHOPApp.swift
+//  xHOP
+//
+//  Created by Ed Penano on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct xHOPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
